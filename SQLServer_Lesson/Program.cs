59116d6fae0f840f3a732df1404b54d0bfadae50
shimanamisan <mikan.sup.all@gmail.com>
2021-06-01 07:01:56 +0900

    [update] アプリケーション全体で受け付ける例外処理を追加

--- SQLServer_Lesson/Program.cs
+++ SQLServer_Lesson/Program.cs
@@ -14,9 +14,18 @@ namespace SQLServer_Lesson
         [STAThread]
         static void Main()
         {
+            // まとめて一括して例外を受け取る
+            Application.ThreadException += Application_ThreadException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        // まとめて一括して例外を受け止めている
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
         }
     }
 }
