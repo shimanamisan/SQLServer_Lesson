4feeaa984b85433add045c6f9c42031575077a7a
shimanamisan <mikan.sup.all@gmail.com>
2021-06-01 07:00:58 +0900

    [add] SQLServerから取得したデータを格納するクラス

---
+++ SQLServer_Lesson/ProductEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServer_Lesson
+{
+    // 継承しないクラスにはsealedをつけたほうが良い
+    public sealed class ProductEntity
+    {
+        // Ctrl + Tabキー2回でコンストラクターが作れる
+        // コンストラクターの引数は命名規約上、先頭の文字は小文字にする
+        public ProductEntity(
+            int productId,
+            string productName,
+            int price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+        }
+
+        // select文で取得したものを格納する
+        // 型はデータベースの型と合わせる
+        public int ProductId  { get; }
+        public string ProductName { get; }
+        public int Price { get; }
+    }
+}
